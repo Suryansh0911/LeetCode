319c98b21e98e90be20fd2475100b50ea636c17f
Suryansh <157221638+Suryansh0911@users.noreply.github.com>
2026-08-03 13:46:07 +0530

    Time: 5 ms (99.16%), Space: 14.2 MB (79.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool help(vector<int>& nums, int i, int target){
-        if(target == 0) return true;
-        if(i >= nums.size() || target < 0) return false;
-
-        if(dp[i][target] != -1) return dp[i][target];
-
-        bool pick = help(nums, i+1, target - nums[i]);
-        bool skip = help(nums, i+1, target);
-
-        return dp[i][target] = pick || skip;
-    }
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum=0;
-        for(int x : nums){
-            sum += x;
+        int total_sum=accumulate(nums.begin(),nums.end(),0);
+        if(total_sum%2!=0)return false;
+        int tar=total_sum/2;
+        bitset<10001>bs;
+        bs[0]=1;
+        for(auto ele : nums){
+            bs|=bs<<ele;
         }
-        if(sum % 2 != 0) return false;
-        int target = sum/2;
-        if(dp.empty()){
-            dp.assign(n, vector<int>(target+1, -1));
-        }
-        return help(nums, 0, target);
-    }
+   return bs[tar]; }
 };
