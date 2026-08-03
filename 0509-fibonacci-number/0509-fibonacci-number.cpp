672f0108a60ac4849af86b991709ffc988d7e76a
Suryansh <157221638+Suryansh0911@users.noreply.github.com>
2026-08-03 12:33:47 +0530

    Time: 3 ms (64.92%), Space: 8.4 MB (11.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
+    vector<int> dp;
     int fib(int n) {
-        if(n == 0 || n==1) return n;
-        return fib(n-1) + fib(n-2);
+        if(n==0 || n==1) return n;
+        
+        if(dp.empty()){
+            dp.assign(n+1, -1);
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
