bc58b190bc0e4a60735e09e8d1723b6031e9cea2
Suryansh <157221638+Suryansh0911@users.noreply.github.com>
2026-08-03 12:49:35 +0530

    Time: 0 ms (100%), Space: 8.8 MB (20.65%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dp;
+    int climbStairs(int n) {
+        if(n<=1) return 1;
+
+        if(dp.empty()){
+            dp.assign(n+1, -1);
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+    }
+};
