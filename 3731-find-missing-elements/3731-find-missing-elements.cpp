d5b0d04318da80f9add130492e51942707b5745a
Suryansh <157221638+Suryansh0911@users.noreply.github.com>
2026-08-05 15:30:19 +0530

    Time: 0 ms (100%), Space: 32.3 MB (54.68%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -4,10 +4,13 @@ public:
         int min = *min_element(nums.begin(), nums.end());
         int max = *max_element(nums.begin(), nums.end());
         vector<int> ans;
-        unordered_set<int> s(nums.begin(), nums.end());
+        vector<bool> present(max-min+1, false);
+        for(int num : nums){
+            present[num-min] = true;
+        }
 
         for(int i=min+1; i<=max; i++){
-            if(!s.count(i)){
+            if(!present[i-min]){
                 ans.push_back(i);
             }
         }
