fb10672b3cde696e5615558a3b3fef6ec9fc7f99
Suryansh <157221638+Suryansh0911@users.noreply.github.com>
2026-08-06 14:53:12 +0530

    Time: 5 ms (64.84%), Space: 14.3 MB (88.55%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int help(vector<vector<int>> &dp, vector<int> &prices, int i, int k){
-        if(i == prices.size() || k==0) return 0;
-        if(dp[i][k] != -1) return dp[i][k];
-        if(k%2 == 0){
-            int buy = help(dp, prices, i+1, k-1) - prices[i];
-            int skip = help(dp, prices, i+1, k);
-            return dp[i][k] = max(buy, skip);
-        }
-        int sell = help(dp, prices, i+1, k-1) + prices[i];
-        int skip = help(dp, prices, i+1, k);
-        return dp[i][k] = max(sell, skip);
-
-    }
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        k*=2;
-        vector<vector<int>> dp(n, vector<int>(k+1, -1));
-        return help(dp, prices, 0, k);
+
+        int actions = 2 * k;
+
+        vector<int> next(actions + 1, 0);
+        vector<int> curr(actions + 1, 0);
+
+        for(int i = prices.size() - 1; i >= 0; i--) {
+
+            for(int action = 1; action <= actions; action++) {
+
+                if(action % 2 == 0)
+                    curr[action] = max(-prices[i] + next[action - 1],
+                                       next[action]);
+                else
+                    curr[action] = max(prices[i] + next[action - 1],
+                                       next[action]);
+            }
+
+            next = curr;
+        }
+
+        return next[actions];
     }
 };
